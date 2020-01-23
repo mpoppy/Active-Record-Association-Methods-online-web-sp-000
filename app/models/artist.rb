271c8c18333a9948a2e958baa95356b950350b70
Marc Poppy <poppy.marc@gmail.com>
2020-01-23 04:02:46 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,7 +9,7 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    artist.songs.count
+    self.songs.count
   end
 
   def genre_count
